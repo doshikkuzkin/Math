a25fc214e765c62cbecaf37e6820dfb26584b051
dariak <dariak@playtika.com>
2020-11-12 10:07:39 +0300

    Measure Test

---
+++ Math/Assets/Tests/SqrMagnitudeTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Unity.PerformanceTesting;
+
+namespace Tests
+{
+    public class SqrMagnitudeTest
+    {
+        [Test, Performance]
+        public void SqrMagnitudeSpeedTest()
+        {
+            Measure.Method(() => sqrMagnitude())
+                .WarmupCount(10)
+                .MeasurementCount(1000)
+                .Run();
+        }
+        
+        [Test, Performance]
+        public void DistanceSpeedTest()
+        {
+            Measure.Method(() => distance())
+                .WarmupCount(10)
+                .MeasurementCount(1000)
+                .Run();
+        }
+
+        private static float distance()
+        {
+            return Vector3.Distance(v1, v2);
+        }
+
+        private static float sqrMagnitude()
+        {
+            return Vector3.SqrMagnitude(v2 - v1);
+        }
+        
+        private static Vector3 v1 => new Vector3(Random.value, Random.value, Random.value);
+        private static Vector3 v2 => new Vector3(Random.value, Random.value, Random.value);
+    }
+}
